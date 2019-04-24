64f4419944ad484c404c97960c4609ef716bc996
Abdullah <bhatti9t7@gmail.com>
2019-04-24 16:50:54 +0500

    skype to insta

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
                 <div class="">
                     <a href="#"><i class="fa fa-twitter"></i></a>
                     <a href="#"><i class="fa fa-facebook"></i></a>
-                    <a href="#"><i class="fa fa-skype"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
                     <a href="#"><i class="fa fa-youtube"></i></a>
                     <a href="#"><i class="fa fa-linkedin"></i></a>
                     <a href="tel:+97433322106"><i class="fa fa-phone"></i></a>
