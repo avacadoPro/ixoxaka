2339a5dd9529a847b9e377eb602ad305de6895c8
Abdullah <bhatti9t7@gmail.com>
2019-04-24 17:02:22 +0500

    conflit of icaon

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
                     <a href="#"><i class="fa fa-instagram"></i></a>
                     <a href="#"><i class="fa fa-youtube"></i></a>
                     <a href="#"><i class="fa fa-linkedin"></i></a>
-                    <a href="tel:+97433322106"><i class="fa fa-phone"></i></a>
+                    <a href="tel:+97433322106"><i class="fa fa-phone footerPhone"></i></a>
                     <a href="tel:+97433322106"><i class="fa fa-whatsapp"></i></a>
                 </div>
             </div>
